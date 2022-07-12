2ed4a454885927ab06f338721676b004251a552d
thisd92 <80495413+thisd92@users.noreply.github.com>
2022-07-12 03:50:36 -0300

    add React Hooks - useState

--- DSMETA/frontend/src/components/Form.tsx
+++ DSMETA/frontend/src/components/Form.tsx
@@ -1,20 +1,28 @@
 import './Form.css';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useState } from 'react';
 
 const Form = () => {
+    
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+    const max = new Date();
+    
+    const [minDate, setMinDate] = useState(min);
+    const [maxDate, setMaxDate] = useState(max);
+
     return (
         <div className="formulario">
             <form className="form" action="">
                 <DatePicker
-                    selected={new Date()}
-                    onChange={(date: Date) => { }}
+                    selected={minDate}
+                    onChange={(date: Date) => setMinDate(date)}
                     className="input-data"
                     dateFormat="dd/MM/yyyy"
                 />
                 <DatePicker
-                    selected={new Date()}
-                    onChange={(date: Date) => { }}
+                    selected={maxDate}
+                    onChange={(date: Date) => setMaxDate(date)}
                     className="input-data"
                     dateFormat="dd/MM/yyyy"
                 />
