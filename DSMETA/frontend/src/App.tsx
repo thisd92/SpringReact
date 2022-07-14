8a0a7c5197048105b7e88bc60d9ff37a85eb44de
thisd92 <80495413+thisd92@users.noreply.github.com>
2022-07-14 14:43:59 -0300

    add Toast

--- DSMETA/frontend/src/App.tsx
+++ DSMETA/frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import Button from "./components/Button";
 import Header from "./components/Header";
 import Card from "./components/Card";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (
     <>
+    <ToastContainer />
       <Header />
       <Card />
     </>
